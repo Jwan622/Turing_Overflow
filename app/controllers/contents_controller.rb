4b246e6decc20d98e6cff6cc70e741ad0f528d6d
Rich Shea <richcshea@gmail.com>
2015-01-18 16:23:30 -0700

    add form to add content to topic

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -3,4 +3,23 @@ class ContentsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+    @content = topic.contents.new(content_params)
+    @content.save!
+    redirect_to @content.topic
+  end
+
+  private
+
+  def topic
+    Topic.find(params[:topic_id])
+  end
+
+  def content_params
+    params.require(:content).permit(:content)
+  end
+
 end
