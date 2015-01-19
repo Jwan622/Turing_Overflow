b7b77ec20635049431814082da615fadadb9beb7
Jeffrey Wan <Jwan622@gmail.com>
2015-01-19 09:50:55 -0700

    finished delete button on topics page

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -12,22 +12,30 @@ class ContentsController < ApplicationController
 
 
   def create
-    require 'pry' ; binding.pry
-    @content = topic.contents.new(content_params)
+    @content = topic_params_id.contents.new(content_params)
     @content.save!
     redirect_to @content.topic
   end
 
   def update
-    @content = Content.find(params[:id])
-    @content.update_attribute(:resource, content_params[:resource])
-    redirect_to topic_path(@content.topic)
+    content_params_id.update_attribute(:resource, content_params[:resource])
+    redirect_to topic_path(content_params_id.topic)
+  end
+
+  def destroy
+    content_params_id.destroy
+    flash[:success] = "Resource deleted. It's gone forever."
+    redirect_to topic_path(topic_params_id)
   end
 
   private
 
-  def topic
-    Topic.find(params[:topic_id])
+  def topic_params_id
+    Topic.find(params[:topic_id].to_i)
+  end
+
+  def content_params_id
+    Content.find(params[:id].to_i)
   end
 
   def content_params
